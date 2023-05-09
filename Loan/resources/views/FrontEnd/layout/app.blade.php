db7a16de924b0750b3a077c8450a6ee46f14e27b
Long-Software <senglongsoftware@gmail.com>
2023-05-09 19:22:12 +0700

    header, footer

--- Loan/resources/views/FrontEnd/layout/app.blade.php
+++ Loan/resources/views/FrontEnd/layout/app.blade.php
@@ -1,6 +1,19 @@
-<header>
-@include('Frontend.layout.header')
-</header>
-<footer>
-@include('Frontend.layout.footer')
-</footer>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @yield('head')
+</head>
+
+<body>
+    <header>
+        @include('Frontend.layout.header')
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        @include('Frontend.layout.footer')
+    </footer>
+</body>
+</html>
--- Loan/routes/web.php
+++ Loan/routes/web.php
@@ -22,9 +22,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 // Route for User
+Route::get('/u', function() {
+    return view('welcome');
+});
+Route::get('/u/request-loan', function() {
+    return view('Frontend.requestLoan');
+});
 
 // Route for Admin
-
 Route::get('/a', function() {
     return view('Backend.dashboard');
 })->name('admin');
@@ -44,5 +49,6 @@ Route::get('/a/customer', function() {
 Route::get('/logins', function() {
     return view('FrontEnd.login');
 });
+
 
 require __DIR__.'/auth.php';
