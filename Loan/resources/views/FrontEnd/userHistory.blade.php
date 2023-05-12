9f403384f5fb3ffbb231efb92bbef7a17c65b4a4
Sovathara Prom <106222937+SovatharaProm@users.noreply.github.com>
2023-05-12 12:29:04 +0700

    Update userHistory.blade.php

--- Loan/resources/views/FrontEnd/userHistory.blade.php
+++ Loan/resources/views/FrontEnd/userHistory.blade.php
@@ -1,1 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="styles.css">
+	<title>History</title>
+</head>
+<body>
+	
+</body>
+</html>
 
+
+
+<html>
+<head>
+	<title>History</title>
+	<style>
+		table, th, td {
+			border: 1px solid black;
+			border-collapse: collapse;
+			padding: 10px;
+		}
+	</style>
+</head>
+<body>
+	<h1>History</h1>
+	<table>
+		<tr>
+			<th>Name</th>
+			<th>Lend Info</th>
+			<th>Payment</th>
+		</tr>
+		<tr>
+			<td>John Doe</td>
+			<td>1000$</td>
+			<td>555-1234</td>
+		</tr>
+		<tr>
+			<td>Jane Doe</td>
+			<td>2000$</td>
+			<td>555-5678</td>
+		</tr>
+		<tr>
+			<td>Bob Smith</td>
+			<td>2000$</td>
+			<td>555-9012</td>
+		</tr>
+	</table>
+</body>
+</html>
