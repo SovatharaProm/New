2ad3467dec9b70f33b74024b2d41929a20589cc2
Long-Software <senglongsoftware@gmail.com>
2023-05-20 13:22:06 +0700

    edid content in main.blade.php

--- Loan/resources/views/FrontEnd/main.blade.php
+++ Loan/resources/views/FrontEnd/main.blade.php
@@ -1,8 +1,102 @@
 @extends ('FrontEnd.layout.app')
-@section ('head')
-<title>Main Homepage</title>
+@section('head')
+    <title>Main Homepage</title>
 @endsection
 
-@section ('content')
+@section('content')
+    <div class="container">
+        <h2 class="text-center py-5">Loans</h2>
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">Basic</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$0<small class="text-body-secondary fw-light">/mo</small>
+                        </h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>Min. Amount</li>
+                            <li>Max. Amount</li>
+                            <li>Rates</li>
+                            <li>Bank name</li>
+                        </ul>
+                        <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">Pro</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$15<small class="text-body-secondary fw-light">/mo</small>
+                        </h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>Min. Amount</li>
+                            <li>Max. Amount</li>
+                            <li>Rates</li>
+                            <li>Bank name</li>
+                        </ul>
+                        <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm border-primary">
+                    <div class="card-header py-3 text-bg-primary border-primary">
+                        <h4 class="my-0 fw-normal">premiere</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$29<small class="text-body-secondary fw-light">/mo</small>
+                        </h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>Min. Amount</li>
+                            <li>Max. Amount</li>
+                            <li>Rates</li>
+                            <li>Bank name</li>
+                        </ul>
+                        <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="container marketing">
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading fw-normal lh-1">Bank Name</h2>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In reiciendis est iusto, commodi vel laudantium omnis quisquam tenetur accusamus nihil.</p>
+            </div>
+            <div class="col-md-5">
+                <img src="" alt="Image here">
+            </div>
+        </div>
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading fw-normal lh-1">Bank Name</h2>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In reiciendis est iusto, commodi vel laudantium omnis quisquam tenetur accusamus nihil.</p>
+            </div>
+            <div class="col-md-5">
+                <img src="" alt="Image here">
+            </div>
+        </div>
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading fw-normal lh-1">Bank Name</h2>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In reiciendis est iusto, commodi vel laudantium omnis quisquam tenetur accusamus nihil.</p>
+            </div>
+            <div class="col-md-5">
+                <img src="" alt="Image here">
+            </div>
+        </div>
+    </div>
 @endsection
