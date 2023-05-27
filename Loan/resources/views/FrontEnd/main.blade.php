4598046eb8b961a8dc20470aa1f12137fe737de2
NAK110 <vathanak110@gmail.com>
2023-05-27 22:30:55 +0700

    make model

--- Loan/resources/views/FrontEnd/main.blade.php
+++ Loan/resources/views/FrontEnd/main.blade.php
@@ -46,7 +46,7 @@
             <div class="col">
                 <div class="card mb-4 rounded-3 shadow-sm border-primary">
                     <div class="card-header py-3 text-bg-primary border-primary">
-                        <h4 class="my-0 fw-normal">premiere</h4>
+                        <h4 class="my-0 fw-normal">Premium</h4>
                     </div>
                     <div class="card-body">
                         <h1 class="card-title pricing-card-title">$29<small class="text-body-secondary fw-light">/mo</small>
